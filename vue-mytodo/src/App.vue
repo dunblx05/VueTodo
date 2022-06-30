b947b8a683a02a7443740f987041e86b41925e5f
dunblx05 <dubblx05@naver.com>
2022-06-30 13:16:37 +0900

    initial commit

---
+++ vue-mytodo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoTitle from "./components/TodoTitle";
+import TodoController from "./components/TodoController";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoTitle,
+    TodoController,
+    TodoList,
+    TodoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
